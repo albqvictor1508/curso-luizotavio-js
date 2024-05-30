8cc54e18062b5a84722e97079956755f80f7b3b5
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-05-30 16:13:47 -0300

    commit

--- JS/objetos/math/math.js
+++ JS/objetos/math/math.js
@@ -10,8 +10,29 @@ for (let comida = 0; comida < array; comida++) {
 
     }
 }
-console.log(menor, 'eu amo alexsa')
+//console.log(menor, 'eu amo alexsa')
 
-console.log(metade)
+//console.log(Math.max(1, 2, 3, 4, 10, 20, 21, 30, 32, 45, 56, 2508))
 
-console.log('\ntotal:',array.length)
+array = [Math.max(1, 2, 3, 4, 10, 20, 21, 30, 32, 45, 56, 2508)]
+
+//console.log(array)
+
+// uma forma de pegar o maior numero do array sem usar condicionais
+
+let aleatorio = Math.round(Math.random() * (10 - 1) + 1)
+console.log(aleatorio)
+
+//random para gerar um número aleatório e round para arredondar e deixá-lo sem casas decimais
+
+let n1 = 10;
+let n2 = 20;
+let n3 = 30;
+let n4 = 40;
+let n5 = 50;
+
+array = [Math.max(n1, n2 ,n3, n4, n5)]
+
+console.log(array)
+
+
--- JS/objetos/math/math.md
+++ JS/objetos/math/math.md
@@ -10,6 +10,17 @@ método(função) round -> arredonda automáticamente para o mais próximo
 
 método(função) max -> encontra o maior número (muito utilizado em arrays)
 
-maior = arr[0]
+Exemplo do Math.max em um array
 
-Math.max(arr)
+    maior = [Math.max(1, 2, 3 ,4, 80)]
+
+Math.min -> faz o contrário do math.max, pegando o menor número 
+
+método (função) random -> cria um número aleatório 
+
+Math.pi -> pega o valor de pi 
+
+Math.pow(2,2) -> potenciação (elevar um numero a outro)
+
+
+
