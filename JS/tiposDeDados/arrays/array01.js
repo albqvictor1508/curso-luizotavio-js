52b96b4bd2126d3ec668b5c4b9670876034760fa
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-05-31 16:21:06 -0300

    commit

--- JS/tiposDeDados/arrays/array01.js
+++ JS/tiposDeDados/arrays/array01.js
@@ -5,8 +5,24 @@ arr[arr.length] = 'vito' //ou array.push('vito')
 arr[arr.length] = 'teste', 'b', 'c' //não funciona (só adicionar o primeiro elemento)
 
 arr.push('a', 'b', 'c') //funciona (adiciona todos)
-console.log(arr)
+
 
 //OS DOIS ADICIONAM NO FIM
 
-arr[7] = 'alterado'
+arr[8] = 'alterado' //altera
+
+
+arr.unshift('eu amo alexsa') //adiciona no começo
+
+//arr.pop() //remove o ultimo elemento
+
+//arr.shift() //remove o primeiro elemento
+
+arr.slice(0, 3) //cortará o array, pegando somente 3 elementos 
+
+//o (0, 3) funcionará como uma subtração, se colocar do (1, 5) por exemplo, só será exibido 4 elementos, (2, 5) = 3 elementos, e assim por diante
+
+console.log(arr.slice(2, 5)) 
+
+
+
