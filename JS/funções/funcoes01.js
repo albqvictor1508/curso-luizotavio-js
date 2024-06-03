ca0f0ba2621fcb830bd032981ab45997da607eab
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-06-03 12:13:51 -0300

    Update funcoes01.js

--- JS/funções/funcoes01.js
+++ JS/funções/funcoes01.js
@@ -3,11 +3,21 @@ const calculo = function(x, y) {
     return x + y
 };
 
-console.log(calculo(10, 20));
+//console.log(calculo(10, 20));
 
-console.log(calculo('victor', ' e alexsa'))
+//console.log(calculo('victor', ' e alexsa'))
 
-const array = [1 ,2 , 8, 10, 9, 25]
+const raiz = function(n) {
+    return n ** 0.5
+}
+
+console.log(raiz(49))
+
+const quadrado = (v , a=2) => {
+    return Math.pow(v , a)
+}
+
+console.log(quadrado(3, 3))
 
 
 
