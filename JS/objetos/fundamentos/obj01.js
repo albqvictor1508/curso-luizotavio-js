abf72d48d821d020960e95bf17bc484f00ff7811
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-06-04 08:21:49 -0300

    commit

--- JS/objetos/fundamentos/obj01.js
+++ JS/objetos/fundamentos/obj01.js
@@ -1,7 +1,7 @@
 const obj = {nome:'Alexsa', sobrenome:'Martins',idade:16,nascimento:15082007}
 
 
-console.log(obj);
+//console.log(obj);
 
 //OBJETOS NÃO PODEM SER MANIPULADOS COM PUSH, POP, SHIFT OU UNSHIFT
 
@@ -12,13 +12,25 @@ const funcaoObjeto = (nome, sobrenome, idade) => {
         nome,
         sobrenome,
         idade,
+    
+        }
+    }
 
 
+const objetin = {
+    nome:'vito',
+    sobrenome:'arruda',
+    idade:16,
+
+    teste() {
+       console.log(`${this.nome} de sobrenome ${this.sobrenome}, de ${this.idade} anos`);
     }
 }
-
 //Assim como no array precisa de uma virgula no final do elemento, no objeto também precisa 
 
-console.log(funcaoObjeto('victor', 'albuquerque', 17))
-console.log(funcaoObjeto('alexsa', 'martins', '16 anos'))
-console.log(funcaoObjeto('lula', 'da silva', 'não sei'))
+//Dentro de uma função podem ser puxados os dados do objeto usando 'this.dado'
+objetin.teste();
+
+//console.log(funcaoObjeto('victor', 'albuquerque', 17))
+//console.log(funcaoObjeto('alexsa', 'martins', '16 anos'))
+//console.log(funcaoObjeto('lula', 'da silva', 'não sei'))
--- JS/objetos/fundamentos/objetos.md
+++ JS/objetos/fundamentos/objetos.md
@@ -1,5 +1,11 @@
 # Objetos
 
+### Sobre objetos
+
+Os objetos não podem ser manipulados com 'push, pop, shift, unshift ou slice'
+
+Ou seja, a única forma de alterar dados dentro de um objeto é uma função ou trocando manualmente esse objeto.
+
 ![alt text](image.png)
 
 um objeto possui a mesma estrutura de um array, porém feito em chaves
@@ -20,3 +26,20 @@ foi criada essa arrow function que retorna um objeto com os argumentos lançados
     }
 
 também pode ser escrito dessa forma
+
+<strong>Objetos podem conter funções, Assim como os arrays</strong>
+
+funções que são criadas sem a palavra function e são utilizadas como um método
+
+    const obj = {
+        nome:'vito',
+        sobrenome:'arruda'
+        idade:17,
+
+        teste() {
+            console.log('pegou');
+        }
+    }
+    obj.teste();
+
+Mesmo que tenha como ser feito em arrays e objetos, geralmente não tem porque utilizar isso 
