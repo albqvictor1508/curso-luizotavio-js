dff52fee843f68e7df6da7264bf1ae968067678d
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-06-06 03:21:46 -0300

    Update main.js

--- JS/exercicios/ex02/assets/js/main.js
+++ JS/exercicios/ex02/assets/js/main.js
@@ -4,34 +4,46 @@ function escopo() {
     
     form.addEventListener('submit', function(event) {
     event.preventDefault();
-    const peso = document.getElementById('peso');
-    const altura = document.getElementById('altura');
+    const peso = Number(document.getElementById('peso'));
+    const altura = Number(document.getElementById('altura'));
     const conta = (peso.value / altura.value) * peso.value
+    let alertaPeso;
+    //Seria ainda mais fácil se feito por um array, e só manipulasse os indices na condição
+
     
-    function AtualizarDiv() {
-        if (conta <= 18.5) {
-            alert('Abaixo do peso')
+    function condicao() {
+        if (conta > 18.5) {
+            alertaPeso = '(Abaixo do peso)';
         }
-        else if (conta < 18.5 && conta > 24.9) {
-            alert('Peso normal')
+        else if (conta <= 18.5 && conta >= 24.9) {
+            alertaPeso = '(Peso normal)';
         }
-        else if (conta > 25 && conta < 29.9) {
-            alert('Sobrepeso')
+        else if (conta >= 25 && conta <= 29.9) {
+            alertaPeso = '(Sobrepeso)';
         }
-        else if (conta > 30 && conta < 34.9) {
-            alert('Obesidade grau 1')
+        else if (conta >= 30 && conta <= 34.9) {
+            alertaPeso = '(Obesidade grau 1)';
         }
-        else if (conta > 35 && conta < 39.9) {
-            alert('Obesidade grau 2')
+        else if (conta >= 35 && conta <= 39.9) {
+            alertaPeso = '(Obesidade grau 2)';
         } 
-        else if (conta > 40) {
-            alert('Obesidade grau 3')
+        else if (conta >= 40) {
+            alertaPeso = '(Obesidade grau 3)';
         }
-        console.log('pegou')
+        else {
+            alert('ERRO!')
+        }
+        //também poderia utilizar uma variável com OR para realizar essa condição
+
+        //ou poderia fazer uma função composta e não utilizar o else if 
+        
     }
-    AtualizarDiv();
+    function atualizarDiv() {
+        res.innerHTML = `Seu IMC é ${conta.toFixed(2)} ${alertaPeso}`
+    }
+    condicao();
+    atualizarDiv();
 });
 }
 escopo()
-alert('teste')
 
