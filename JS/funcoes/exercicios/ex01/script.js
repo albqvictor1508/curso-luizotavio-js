4d3c1df103a77ff4e18955382adb64a0cd77f86c
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-06-17 15:58:01 -0300

    commit

--- JS/funcoes/exercicios/ex01/script.js
+++ JS/funcoes/exercicios/ex01/script.js
@@ -5,7 +5,14 @@ function escopo() {
     const btnReset = document.getElementById('reset');
     let segundos = 0;
     let minutos = 0;
-    let horas = 0; 
+    let milisegundos = 0; 
+
+    btnStart.addEventListener('click',function() {
+        ativarTimer();
+        setInterval(ativarTimer,20)
+    });
+    btnPause.addEventListener('click',pausarTimer);
+    btnReset.addEventListener('click',zerarTimer);
 
     function formatarTempo(valor) {
     return valor < 10? `0${Number(valor)}`: valor;
@@ -13,22 +20,38 @@ function escopo() {
     }
 
     
-    function criarTimer() {
-        segundos++;
+    function ativarTimer() {
+        milisegundos++;
+        if (milisegundos === 100) {
+            segundos++;
+            milisegundos = 0;
+        }
         if (segundos === 60) {
             minutos++;
             segundos = 0;
         }
-        if (minutos === 60) {
-            horas++;
-            minutos = 0;
-        }
-        console.log(formatarTempo(horas), formatarTempo(minutos), formatarTempo(segundos))
+        
+        mostrarRelogio();
+
     }
-    setInterval(criarTimer, 1000)
     
-    function mostrarRelogio() {
-        relogio.innerHTML = `${formatarTempo(horas)}:${formatarTempo(minutos)}:${formatarTempo(segundos)}` 
+    function pausarTimer(palse) {
+        palse = true
+        clearInterval(ativarTimer);
+        relogio.style.color = `red`;
     }
+    
+    function zerarTimer() {
+        clearInterval(ativarTimer)
+        minutos = 0;
+        segundos = 0;
+        milisegundos = 0;
+    }
+    function mostrarRelogio() {
+        
+        relogio.innerHTML = `${formatarTempo(minutos)}:${formatarTempo(segundos)}:${formatarTempo(milisegundos)}`
+    }
+    setInterval(mostrarRelogio,20)
+    
 }
 escopo();
