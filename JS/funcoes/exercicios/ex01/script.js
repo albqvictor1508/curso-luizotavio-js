badb273bedc390c8520b0206998bba9df88c94f0
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-06-17 11:39:40 -0300

    commit

--- JS/funcoes/exercicios/ex01/script.js
+++ JS/funcoes/exercicios/ex01/script.js
@@ -1,11 +1,34 @@
 function escopo() {
-    const relogio = document.querySelector('#relogio');
-    const data = new Date();
+    const relogio = document.querySelector('.relogio');
+    const btnStart = document.getElementById('start');
+    const btnPause = document.getElementById('pause');
+    const btnReset = document.getElementById('reset');
+    let segundos = 0;
+    let minutos = 0;
+    let horas = 0; 
 
-    function AtualizarRelogio() {
-    relogio.innerHTML = `${data.getMinutes}:${data.getSeconds}:${data.getMilliseconds}`;
+    function formatarTempo(valor) {
+    return valor < 10? `0${Number(valor)}`: valor;
+
     }
-    AtualizarRelogio();
 
+    
+    function criarTimer() {
+        segundos++;
+        if (segundos === 60) {
+            minutos++;
+            segundos = 0;
+        }
+        if (minutos === 60) {
+            horas++;
+            minutos = 0;
+        }
+        console.log(formatarTempo(horas), formatarTempo(minutos), formatarTempo(segundos))
+    }
+    setInterval(criarTimer, 1000)
+    
+    function mostrarRelogio() {
+        relogio.innerHTML = `${formatarTempo(horas)}:${formatarTempo(minutos)}:${formatarTempo(segundos)}` 
+    }
 }
 escopo();
