5132e6b812afbca8b384b15a1cdbf5726e1ad67b
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-06-19 13:18:46 -0300

    commit

--- JS/funcoes/exercicios/ex02/script.js
+++ JS/funcoes/exercicios/ex02/script.js
@@ -3,39 +3,56 @@ function escopo() {
     const btnEnviar = document.querySelector('.Enviar');
     const array = []; 
     const ul = document.querySelector('#tarefas');
-    const btnApagar = document.getElementById('btnApagar')
+
 
     
     btnEnviar.addEventListener('click',adicionarArray);
-    btnApagar.addEventListener('click',retirarArray);
     
     function adicionarArray() {
         let tarefas = inputTarefas.value;
-        array.push(tarefas)
-        console.log(array);
+        
         if (tarefas) {
+            array.push(tarefas);
+            inputTarefas.value = ''; //zerar o input quando enviar
             mostrarTarefas();
         }
     }
-
-    function retirarArray() {
-        array.pop();
+    
+    function retirarArray(indice) {
+    //com o parâmetro passado no addEventListener, o botão acionado vai apagar a exata tarefa em que foi apertado
+        array.splice(indice, 1);
         mostrarTarefas();
-        console.log(`POP: ${array}`)
+
     }
-
-
-
     function mostrarTarefas() {
-        const item = document.createElement('li');
-        for (let i in array) {
-            item.textContent = array[i];
+        ul.innerHTML = '';
+        //poderia ser tanto esse for, quanto um forEach.
+        
+        //nesse caso é mais facil um forEach
+        
+        array.forEach((tarefa, indice) => {
+            //sequencia padrão do forEach: primeiro vem os valores, depois os indices, depois o array completo.
+
+            //tarefa armazena os valores do array, indice armazena os indices.
+            const item = document.createElement('li');
+            item.textContent += tarefa;
+            //para cada valor é criada uma li e pos
+            
+            const btnApagar = document.createElement('button');
+            btnApagar.textContent = `Apagar.`;
+            btnApagar.addEventListener('click', () => retirarArray(indice));
+            // Chama 'retirarArray' passando o parâmetro que está armazenando os indices do array
+            
+            item.appendChild(btnApagar);
             ul.appendChild(item);
-        }
 
+        });
+        //sintaxe correta do forEach.
     }
-    adicionarArray();
 
-//Preciso de uma forma de tirar esse valor inicial do array e criar o botão de apagar dentro de cada li para ativar a função retirarArray
+    
+    //Preciso de uma forma de tirar esse valor inicial do array e criar o botão de apagar dentro de cada li para ativar a função retirarArray
 }
 escopo();
+
+//revisar código
