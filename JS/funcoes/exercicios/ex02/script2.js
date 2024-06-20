9b4c8f820a02a7a87c7c1ad97cd947386ec868c0
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-06-20 15:56:01 -0300

    commit

--- JS/funcoes/exercicios/ex02/script2.js
+++ JS/funcoes/exercicios/ex02/script2.js
@@ -1,21 +1,54 @@
 function escopo() {
     const inputTarefas = document.querySelector('#inputTarefas');
     const btnEnviar = document.querySelector('.Enviar');
-    const tarefas = inputTarefas.value;
+    const ul = document.getElementById('tarefas');
+    const array = [];
+    
     btnEnviar.addEventListener('click', function(event) {
-        if (tarefas) {
-            criaTarefa(tarefas);
-        }
+        criaTarefa(inputTarefas.value);
+    //Dessa forma pegou, utilizando uma variável com esse valor não pegou.
     })
     inputTarefas.addEventListener('keypress',function(event) {
         if (event.key === 'Enter') {
-            criaTarefa(tarefas);
+            criaTarefa(inputTarefas.value);
         }
     })
 
-    function criaTarefa(text) {
-        console.log(text);
+    function criaLi(tarefa, indice) {
+        const item = document.createElement('li');
+        item.textContent = tarefa; 
+
+        const btnApagar = document.createElement('button');
+        btnApagar.textContent = 'Apagar';
+        btnApagar.addEventListener('click', () => retirarArray(indice));
+
+        item.appendChild(btnApagar);
+        ul.appendChild(item);
+
+        LimparInput();
+    }
+    function criaTarefa(tarefas) {
+        if (tarefas) {
+            array.push(tarefas);
+            mostrarTarefas();
+          
+        }
+    }
+    function LimparInput() {
+        inputTarefas.value = ``;
     }
 
+    function mostrarTarefas() {
+        ul.innerHTML = '';
+        array.forEach((tarefa,indice) => {
+            criaLi(tarefa,indice);
+        });
+    }
+
+    function retirarArray(index) {
+        array.splice(index,1);
+        mostrarTarefas();
+        
+    }
 }
 escopo();
