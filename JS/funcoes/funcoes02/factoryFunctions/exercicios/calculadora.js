21e8eff51cacc9cb73c093f08b599329c6869bcc
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-06-29 16:29:16 -0300

    commit

--- JS/funcoes/funcoes02/factoryFunctions/exercicios/calculadora.js
+++ JS/funcoes/funcoes02/factoryFunctions/exercicios/calculadora.js
@@ -1,47 +1,42 @@
 (function() {
-    const tela = document.getElementById('numTela');
-    const btn1 = document.querySelector('#um');
-    btn1.addEventListener('click', function() {
-        calculadora(1);
+    const botões = document.querySelectorAll('button');
+    const tela = document.getElementById('telaNum')
+    botões.forEach(botao => {
+        document.addEventListener('click',function(e) {
+            calculadora(parseInt(botao.textContent))
         })
-    const btn3 = document.querySelector('#tres');
-    const btn4 = document.querySelector('#quatro');
-    const btn5 = document.querySelector('#cinco');
-    const btn6 = document.querySelector('#seis');
-    const btn7 = document.querySelector('#sete');
-    const btn2 = document.querySelector('#dois');
+    })
 
-
-    
     function calculadora(operador = '+', acumulador = 0, ...numeros) {
         return {
-            numeros,
             operador,
             acumulador,
+            numeros,
             calculo() {
                 for(numero of numeros) {
-                    if (operador === '+') acumulador += numero;
-                    if (operador === '-') acumulador -= numero;
-                    if (operador === '/') acumulador /= numero;
-                    if (operador === '*') acumulador *= numero;
-                    AtualizarCalc(acumulador);
-                    receberObj(calculadora);
-                    
+                    switch (operador) {
+                        case '+':
+                            acumulador += numero;
+                            break
+                        case '-':
+                            acumulador -= numero;
+                            break
+                        case '/':
+                            acumulador /= numero;
+                            break
+                        case '*':
+                            acumulador *= numero;
+                            break
+
+                        }
+                        executarCalc(calculadora);
                 }
             }
         }
     }
-    
-    function receberObj(obj) {
-        console.log(obj, obj.calculo())
-        return obj.calculo();
-    }
-    
 
-    function AtualizarCalc(valor) {
-        console.log(valor);
-        tela.textContent = valor;
+    function executarCalc(obj) {
+    console.log(obj);
     }
 
-    
-})()
+})();
