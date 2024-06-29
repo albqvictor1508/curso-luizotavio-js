883159315fb86436b8f23dd73b2b4963611ac238
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-06-29 14:14:21 -0300

    122

--- JS/funcoes/funcoes02/factoryFunctions/exercicios/calculadora.js
+++ JS/funcoes/funcoes02/factoryFunctions/exercicios/calculadora.js
@@ -1,16 +1,34 @@
 (function() {
-    const inputCalc = document.getElementById('inputCalculadora');
-    inputCalc.value = 0;
-    inputCalc.addEventListener('keypress', function(e) {
-       if (e.key === '1') {
-        inputCalc.value = 1;
-        AtualizarCalc(inputCalc.value);
-       }
-    })
+    const btn1 = document.querySelector('#um');
+    const btn2 = document.querySelector('#dois');
+    const btn3 = document.querySelector('#tres');
+    const btn4 = document.querySelector('#quatro');
+    const btn5 = document.querySelector('#cinco');
+    const btn6 = document.querySelector('#seis');
+    const btn7 = document.querySelector('#sete');
+
+    
+    function calculadora(operador, acumulador, ...numeros) {
+        return {
+            numeros,
+            operador,
+            acumulador,
+            calculo() {
+                for(numero of numeros) {
+                    if (operador === '+') acumulador += numero;
+                    if (operador === '-') acumulador -= numero;
+                    if (operador === '/') acumulador /= numero;
+                    if (operador === '*') acumulador *= numero;
+                    AtualizarCalc(acumulador);
+                }
+            }
+        }
+    }
+
 
     function AtualizarCalc(valor) {
-        console.log('AtualizarCalc pegou!')
-        inputCalc.textContent = valor;
+        console.log(valor)
+        //inputCalc.textContent = valor;
     }
 
     
