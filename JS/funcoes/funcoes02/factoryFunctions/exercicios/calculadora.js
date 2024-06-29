8e6b9b17bc64766dc3f4aef8e12b410d213b6901
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-06-29 15:50:34 -0300

    commit

--- JS/funcoes/funcoes02/factoryFunctions/exercicios/calculadora.js
+++ JS/funcoes/funcoes02/factoryFunctions/exercicios/calculadora.js
@@ -1,14 +1,19 @@
 (function() {
+    const tela = document.getElementById('numTela');
     const btn1 = document.querySelector('#um');
-    const btn2 = document.querySelector('#dois');
+    btn1.addEventListener('click', function() {
+        calculadora(1);
+        })
     const btn3 = document.querySelector('#tres');
     const btn4 = document.querySelector('#quatro');
     const btn5 = document.querySelector('#cinco');
     const btn6 = document.querySelector('#seis');
     const btn7 = document.querySelector('#sete');
+    const btn2 = document.querySelector('#dois');
+
 
     
-    function calculadora(operador, acumulador, ...numeros) {
+    function calculadora(operador = '+', acumulador = 0, ...numeros) {
         return {
             numeros,
             operador,
@@ -20,15 +25,22 @@
                     if (operador === '/') acumulador /= numero;
                     if (operador === '*') acumulador *= numero;
                     AtualizarCalc(acumulador);
+                    receberObj(calculadora);
+                    
                 }
             }
         }
     }
-
+    
+    function receberObj(obj) {
+        console.log(obj, obj.calculo())
+        return obj.calculo();
+    }
+    
 
     function AtualizarCalc(valor) {
-        console.log(valor)
-        //inputCalc.textContent = valor;
+        console.log(valor);
+        tela.textContent = valor;
     }
 
     
