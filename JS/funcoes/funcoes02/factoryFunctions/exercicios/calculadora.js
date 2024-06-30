a43f04eab448829245caeb68e92f2f0e13e4e4e8
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-06-29 22:14:00 -0300

    123

--- JS/funcoes/funcoes02/factoryFunctions/exercicios/calculadora.js
+++ JS/funcoes/funcoes02/factoryFunctions/exercicios/calculadora.js
@@ -1,29 +1,32 @@
+
 (function() {
     function criarCalc() {
         return {
-            tela: document.querySelector('#telaNum');
+            tela: document.querySelector('#inputCalc'),
 
 
             criarBotões() {
-                const botões = document.querySelectorAll('.num');
                 document.addEventListener('click', function(e) {
                     const clique = e.target;
                     if (clique.classList.contains('num')) {
-                        this.calculo(clique.innerText);
+                        this.iniciar(clique.innerText);
+                        
                     }
                 }.bind(this))//faz o this apontar pro objeto em vez de apontar pro HTML
-            }
+            },
 
-            calculo(texto) {
-                this.tela.value += texto;
+            calculo() {
+                
+            },
 
-            }
-
-            iniciar() {
             
-            }
+            
+            iniciar(texto) {
+                this.tela.value += texto;
+            
+            },
         }
     }
     const objeto = criarCalc();
-    objeto.iniciar();
+    objeto.criarBotões();
 })()
