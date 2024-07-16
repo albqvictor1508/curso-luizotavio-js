872d23b95791fb0c0d5123a91618b23b20535249
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-07-16 19:05:13 -0300

    commit

--- JS/classesJS/classe/validarFormulario/validarform1.js
+++ JS/classesJS/classe/validarFormulario/validarform1.js
@@ -1,54 +1,132 @@
-function criarDOM() {
-    const form = document.querySelector('.formulario');
-    
-    form.addEventListener('submit', function(e) {
-        const el = e.target;
-        if(el.classList.contains('validar')) {
-            criaClasse(el.value);
-        }
-    })
-}
-
-
-
 /* 
-Nenhum campo vazio
-Letras ou numeros
-Senha entre 6 a 12 caracteres
+Um evento de submit para fazer um for querySelectorAll e um for que pegam todos os campos dentro do form.
+
+Fazer uma checagem para que se algum dos campos estiver vazio, o formulario não vai ser enviado.
+
+fazer uma função que cria uma mensagem de erro, coloca uma classe nela e a exibe no HTML.
+
+captar os campos por classe e fazer a checagem 
+
+criar uma variável booleana com 'true' e a alterar para false em cada checagem, se passar por todas essa variável é retornada
+
+Usar alguns métodos para posicionar a div no lugar correto.
+
+criar uma instância da classe CPF e mandar uma mensagem de erro caso seja inválido
+
+Validar o tanto de caracteres o usuário e a senha podem ter 
+
+Validar se a senha e a senha repetida são iguais
+
+Se a função que valida a senha e a função que valida os outros campos forem positivas, envia o formulário
 */
 
-function criaClasse(nome,sobrenome,cpf,usuario,senha, repetirSenha) {
-    class Formulario {
-        constructor(name, sn, cpff, user, password, repetirPassword) {
-            this.nome = name;
-            this.sobrenome = sn;
-            this.cpf = cpff;
-            this.usuario = user;
-            this.senha = password;
-            this.repetirSenha = repetirPassword;
+class ValidaForm {
+        constructor() {
+            this.form = document.querySelector('.formulario');
+            this.formEventos()
         }
 
-        validaForm() {
-            if(this.senha !== this.repetirSenha) return false;
-            if(!this.nome||!this.cpf||!this.sobrenome||!this.usuario||!this.senha) return false;
-            if(this.senha.length < 6||this.senha.length > 12) return false;
-            if(this.usuario.length < 3|| this.usuario.length > 12) return false;
+        formEventos() {
+            //arrow function pra manter o this do objeto e n passar pra função
+            this.form.addEventListener('submit', e => {
+                this.handleSubmit(e);
+            })
         }
+
+        handleSubmit(e) {
+            e.preventDefault();
+            this.validaCampos();
+
+            if(this.validaCampos && this.validaSenha) {
+                this.form.submit();
+            }
+        }
+
+        validaCampos() {
+            let valida = true;
+
+            for(let msgErro of this.form.querySelectorAll('.texto-erro')) {
+                msgErro.remove();
+            }
+            
+
+            for(let campo of this.form.querySelectorAll('.validar')) {
+                const label = campo.previousElementSibling.innerText;
+                if(!campo.value) {
+                    this.msgDeErro(campo, `${label} não pode ficar vazio`);
+                    valida = false
+                }
+
+                //selecionando o campo que tem a classe cpf
+                if(campo.classList.contains('cpf')) {
+                //enviando o vampo como parâmetro pra pegar o valor dele dentro da validaCPF
+                    if(!this.validaCPF(campo)) return valida = false;
+                }
+
+                if(campo.classList.contains('usuario')) {
+                    if(!this.ValidaUsuario(campo))return valida = false;
+                }
+
+                
+            }
+
+        }
+
+        validaCPF(campo) {
+            const cpf = new CPF(campo.value);
+
+            if(!cpf.validadorCPF) {
+                this.msgDeErro(campo, `CPF Inválido`);
+                return false;
+            }
+            return true;
+        }
+
+        ValidaUsuario(campo) {
+            const valor = campo.value;
+
+            if(valor.length < 3|| valor.length > 12) {
+                this.msgDeErro(campo, `Usuário precisa ter entre 3 a 12 caracteres`);
+                return false
+            }
+
+            return true
+        }
+
+        validaSenha(campo) {
+            const valorSenha = this.form.querySelector('.senha');
+            const valorRepetirSenha = this.form.querySelector('.repetir-senha');
+
+            if(valorSenha.length > 6||valorSenha.length < 12) {
+                this.msgDeErro(campo, `Senha deve conter entre 6 a 12 caracteres`);
+                return false;
+            }
+            if(valorSenha !== valorRepetirSenha) {
+                this.msgDeErro(campo, `Senha e Repetir Senha devem ser iguais`);
+                return false
+            }
+            return true;
+        }
+
+        msgDeErro(campo, msg) {
+            const div = document.createElement('div');
+            div.innerHTML = msg;
+            div.classList.add('texto-erro')
+            campo.insertAdjacentElement('afterend', div);
+        }
+
     }
 
-    
-    
+const valida = new ValidaForm();
+
+
     class CPF {
         constructor(cpf) {
             this.cpf = cpf;
             this.cpfLimpo = this.cpf.replace(/\D+/g, '');
         }
-
-        exibir() {
-            console.log(this.cpf);
-        }
         
-        geraCPF() {
+       geraCPF() {
             const CPF9digitos = this.cpfLimpo.slice(0, -2);
             const digito1 = this.geraDigitos(CPF9digitos)
             const digito2 = this.geraDigitos(CPF9digitos + digito1);
@@ -70,23 +148,16 @@ function criaClasse(nome,sobrenome,cpf,usuario,senha, repetirSenha) {
             return calculo > 9? '0': String(calculo);
         }
         
-        get validaCPF() {
+        get validadorCPF() {
             if(!this.cpfLimpo) return false;
             if(this.cpfLimpo.length !== 11) return false;
             if(typeof this.cpfLimpo !== 'string') return false;
-            
-            if(this.geraCPF()) return `CPF válido`;
-            
-            return `CPF inválido`
+
+            if(!this.geraCPF()) return false;
+
+            return true
         }
     }
-    const formulario = new Formulario(nome, sobrenome, usuario, senha, repetirSenha);
-    const cpfFormulario = new CPF('136.063.084-85');
-
-    console.log(cpfFormulario, formulario);
-
-}
-criaClasse();
 
 /* 
 Preciso de lógica para juntar o cpf do formulario, e de lógica para pegar todos os valores quando o formulário der submit.
