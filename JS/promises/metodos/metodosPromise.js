3ecd69e1a69f807f9b228b32630d4c064260a821
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-07-17 15:02:25 -0300

    commit

--- JS/promises/metodos/metodosPromise.js
+++ JS/promises/metodos/metodosPromise.js
@@ -1,12 +1,12 @@
 function criaTarefa(tarefa, tempo) {
     return new Promise((resolve, reject) => {
-        if(typeof tarefa !== 'string') {
-            reject('se tiver erro o promise.all vai retornar o erro');
-            return false;
-        }
-
-
+        
+        
         setTimeout(() => {
+            if(typeof tarefa !== 'string') {
+                reject('ERRO CUZAO');
+                return false;
+            }
             resolve(tarefa + ' - promisse');
         }, tempo)
     })
@@ -21,6 +21,7 @@ function PromiseAll() {
         'Ultimo valor (promessa resolvida)',
     ];
 
+    //se tiver um erro, vai retornar o erro, se não tiver, vai retornar todas as promisses.
     Promise.all(promessas)
     .then(tarefa => {
         console.log(`TAREFAS RESOLVIDAS\n`);
@@ -30,8 +31,43 @@ function PromiseAll() {
 
 //mesmo que retorne o erro, ele ainda executa todas as outras promises
 }
-PromiseAll();
+//PromiseAll();
+
+
 
 function promiseRace() {
+    const promessas = [
+        'vou ser escolhido',
+        criaTarefa('dormir', 1000),
+        criaTarefa('acordar', 2000),
+        criaTarefa('treinar', 2500),
+        criaTarefa('viver o amor', 5),
+        criaTarefa('deixar o futuro mostrar oque tem guardado', 3000),
+        criaTarefa(123, 1),
+    ];
     
+//promice.race vai retornar o primeiro valor.
+
+//mesmo depois de retornar o primeiro valor, ele ainda vai executar os tempos dos outros valores
+    Promise.race(promessas)
+    .then(promessa => console.log(promessa))
+    .catch(e => console.log(`peguei o erro: ${e}`))
 }
+//promiseRace();
+
+function PromiseResolveReject() {
+    let jogo = true;
+    if(typeof jogo !== 'boolean') {
+        jogo = false;
+        return Promise.reject('vai toma no cu irmao');
+    }
+
+    if(jogo) return Promise.resolve(`já tenho hehe`);
+
+    else {
+        return criaTarefa('Baixei o jogo', 2500)
+    }
+}
+PromiseResolve()
+.then(jogo => console.log(jogo))
+.catch(e => console.log(e))
--- JS/promises/metodos/metodosPromise.md
+++ JS/promises/metodos/metodosPromise.md
@@ -2,8 +2,20 @@
 
 ### Promise.resolve
 
+cria uma promessa ja resolvida
+
 ### Promise.race
+
+retorna o primeiro valor em um array de promessas
+
+mesmo depois de retornar o valor, executa o tempo dos outros valores desse array
 
 ### Promise.all
 
+retorna todas as promessas em um array de promessas
+
+Se tiver um erro entre as promessas, retorna o erro
+
 ### Promise.reject
+
+cria uma promessa com erro
