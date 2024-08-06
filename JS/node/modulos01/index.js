53e71492efefc5a00ead178b5c74652f72da5b5b
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-08-06 06:59:33 -0300

    commit

---
+++ JS/node/modulos01/index.js
@@ -0,0 +1,10 @@
+const primeiroModulo = require('./modulo01');
+const {soma} = require('./modulo01');
+
+console.log(primeiroModulo);
+
+console.log(soma);
+
+console.log(primeiroModulo.falaNome('vito', 'arruda'));
+
+console.log(soma(10,10))
---
+++ JS/node/modulos01/modulo01.js
@@ -0,0 +1,25 @@
+const nome = 'lexsa';
+const sobrenome = 'maravilhosa de vito';
+const nomeCompleto = () => nome + ' ' + sobrenome;
+
+module.exports.falaNome = (nome, sobrenome) => {
+    return `${nome} ${sobrenome}!!!`;
+};
+//poderia ser exports no lugar de module.exports.
+exports.idade = 18 + ' anos';
+exports.soma = function(x,y) {
+    return x + y;
+}
+//this também pode ser usado
+this.detalhe = 'eu também vou ser exportado';
+
+class Pipou {
+    constructor(nome,sobrenome, idade) {
+        this.nome = nome;
+        this.nome = nome;
+        this.nome = nome;
+    }
+}
+
+
+
