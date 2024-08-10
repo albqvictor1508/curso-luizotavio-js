29fe3dac9e42642cf5157ef371538b1171ca203d
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-08-10 14:56:03 -0300

    commit

--- JS/node/express/expressWebpack/routes.js
+++ JS/node/express/expressWebpack/routes.js
@@ -1,2 +1,8 @@
 const express = require('express');
 const routes = express.Router();
+const homeController = require('./src/controllers/homeController');
+
+
+routes.get('/', homeController.paginaInicial)
+
+module.exports = routes;
