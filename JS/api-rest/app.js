5303b6a657891367b4617e0c9ec5c9e30f5f4c6c
Victor Arruda <albq.victor@gmail.com>
2024-09-06 06:57:58 -0300

    commit

--- JS/api-rest/app.js
+++ JS/api-rest/app.js
@@ -1,3 +1,24 @@
-const teste = 10;
+import dotenv from 'dotenv';
+dotenv.config();
 
-console.log(teste);
+import express from 'express';
+import homeRoutes from './src/routes/homeRoutes';
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use('/', homeRoutes);
+  }
+}
+
+export default new App().app;
