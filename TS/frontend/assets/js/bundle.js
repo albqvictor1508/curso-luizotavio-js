d2e8becaf1481ca032ebb9874d9c9a05b43a8f60
Victor Arruda <albq.victor@gmail.com>
2024-11-30 12:01:55 -0300

    JS setup completed

--- TS/frontend/assets/js/bundle.js
+++ TS/frontend/assets/js/bundle.js
@@ -4,6 +4,24 @@
   !*** ./src/webpack/index.ts ***!
   \******************************/
 
+(() => {
+    const form = document.querySelector("#form");
+    const usernameInput = document.querySelector("#usernameInput");
+    const emailInput = document.querySelector("#emailInput");
+    const PasswordInput = document.querySelector("#passwordInput");
+    const repeatPasswordInput = document.querySelector("#password2Input");
+    form === null || form === void 0 ? void 0 : form.addEventListener("click", e => {
+        e.preventDefault();
+        if (handleSubmit()) {
+            form.submit();
+        }
+        function handleSubmit() {
+            let valid = true;
+            return valid;
+        }
+    });
+})();
 
 /******/ })()
 ;
+//# sourceMappingURL=bundle.js.map
--- TS/src/webpack/index.ts
+++ TS/src/webpack/index.ts
@@ -1,1 +1,22 @@
-console.log("testando porra");
+(() => {
+  const form: HTMLFormElement | null = document.querySelector("#form");
+  const usernameInput: HTMLInputElement | null = document.querySelector("#usernameInput");
+  const emailInput: HTMLInputElement | null = document.querySelector("#emailInput");
+  const PasswordInput: HTMLInputElement | null = document.querySelector("#passwordInput");
+  const repeatPasswordInput: HTMLInputElement | null = document.querySelector("#password2Input");
+
+  form?.addEventListener("click", e => {
+    e.preventDefault();
+    if(handleSubmit()) {
+      form.submit();
+    }
+
+    function handleSubmit(): boolean {
+      let valid = true;
+
+
+
+      return valid
+    }
+  })
+})()
