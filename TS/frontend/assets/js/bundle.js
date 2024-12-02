c65d74cfc6d5470fbd5787179b26b5308e4f945b
albqvictor1508 <victorarruda460@gmail.com>
2024-12-02 03:20:48 -0300

    form validation almost ready

--- TS/frontend/assets/js/bundle.js
+++ TS/frontend/assets/js/bundle.js
@@ -6,17 +6,29 @@
 
 (() => {
     const form = document.querySelector("#form");
-    const usernameInput = document.querySelector("#usernameInput");
-    const emailInput = document.querySelector("#emailInput");
-    const PasswordInput = document.querySelector("#passwordInput");
-    const repeatPasswordInput = document.querySelector("#password2Input");
-    form === null || form === void 0 ? void 0 : form.addEventListener("click", e => {
+    const formFields = form === null || form === void 0 ? void 0 : form.querySelectorAll("input");
+    form === null || form === void 0 ? void 0 : form.addEventListener("submit", e => {
         e.preventDefault();
         if (handleSubmit()) {
             form.submit();
         }
         function handleSubmit() {
             let valid = true;
+            const errorMessagesArray = document.querySelectorAll(".error-message");
+            if (errorMessagesArray) {
+                errorMessagesArray.forEach(msg => {
+                    for (const input of formFields) {
+                        if (!input.value) {
+                            msg.textContent = "All the field must to be filled";
+                            input.style.borderColor = "red";
+                            valid = false;
+                        }
+                    }
+                });
+            }
+            if (!valid) {
+                alert(`form not sended`);
+            }
             return valid;
         }
     });
--- TS/src/webpack/index.ts
+++ TS/src/webpack/index.ts
@@ -1,22 +1,37 @@
-(() => {
-  const form: HTMLFormElement | null = document.querySelector("#form");
-  const usernameInput: HTMLInputElement | null = document.querySelector("#usernameInput");
-  const emailInput: HTMLInputElement | null = document.querySelector("#emailInput");
-  const PasswordInput: HTMLInputElement | null = document.querySelector("#passwordInput");
-  const repeatPasswordInput: HTMLInputElement | null = document.querySelector("#password2Input");
+  (() => {
+    const form: HTMLFormElement | null = document.querySelector("#form");
+    const formFields: any = form?.querySelectorAll("input");
 
-  form?.addEventListener("click", e => {
-    e.preventDefault();
-    if(handleSubmit()) {
-      form.submit();
-    }
+    form?.addEventListener("submit", e => {
+      e.preventDefault();
+      if(handleSubmit()) {
+        form.submit();
+      }
 
-    function handleSubmit(): boolean {
-      let valid = true;
+      
+      function handleSubmit(): boolean {
+        let valid = true; 
+        const errorMessagesArray: NodeListOf<HTMLSpanElement> = document.querySelectorAll(".error-message");
+
+        if(errorMessagesArray) {
+          errorMessagesArray.forEach(msg => {
+            for(const input of formFields) {
+              if(!input.value) {
+                msg.textContent = "All the field must to be filled";
+                input.style.borderColor = "red";
+
+                valid = false;
+              }
+            }
+          })
+        }
+        
+        if(!valid) {
+          alert(`form not sended`);
+        }
 
 
-
-      return valid
-    }
-  })
-})()
+        return valid;
+      }
+    })
+  })()
