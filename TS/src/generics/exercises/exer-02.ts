ee3bb19727782d659524120365ca5f76df5d2526
albqvictor1508 <albq.victor@gmail.com>
2025-02-13 14:28:09 -0300

    index signature and array & promisses

--- TS/src/generics/exercises/exer-02.ts
+++ TS/src/generics/exercises/exer-02.ts
@@ -1,1 +1,23 @@
-const bau: <T>(name: T) => string = (name) => `Olá, ${name}`;
+const dev = {
+	name: "John Doe",
+	salary: 999,
+};
+
+const dev2 = {
+	name: "Cuca Beludo",
+	salary: 1000,
+};
+
+function devsSalary(salary: { salary: number }) {
+	console.log(`Seu salário é: ${salary}`);
+}
+
+devsSalary(dev);
+
+function devsSalary2(salaryObj: { [key: string]: number | string }) {
+	console.log(`Seu salário é: ${salaryObj.salary} (index signature)`);
+}
+
+devsSalary2(dev2);
+
+//a primeira forma tipa somente a chave salary, a segunda forma tipa todo o objeto, chamada de index signature
