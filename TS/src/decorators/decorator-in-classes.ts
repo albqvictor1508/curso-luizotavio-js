2d9bfcd1c06d6d29f3469d190cfe1f4b7daac363
albqvictor1508 <albq.victor@gmail.com>
2025-02-28 17:36:27 -0300

    decorator in classes

--- TS/src/decorators/decorator-in-classes.ts
+++ TS/src/decorators/decorator-in-classes.ts
@@ -1,8 +1,19 @@
+@decorator
 export class Car {
 	constructor(public cor: string) {}
 }
-export function decorator<T>(target: T) {
-	return target;
+export function decorator<T extends new (...args: any[]) => any>(target: T): T {
+	return class extends target {
+		cor: string;
+		name: string;
+
+		constructor(...args: any[]) {
+			super(...args);
+			this.name =
+				"Posso mudar aqui o nome que foi enviado no constructor quando foi instânciado";
+			this.cor = "mudar aq a cor também";
+		}
+	};
 }
-const carDecorated = decorator(Car);
-const car = new carDecorated();
+const carDecorated = decorator<typeof Car>(Car);
+const car = new carDecorated("yellow");
