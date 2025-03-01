6fd71d062d6cdf722740cf3d0db798274bdc92c2
albqvictor1508 <albq.victor@gmail.com>
2025-03-01 13:36:06 -0300

    introducing namespaces

---
+++ TS/src/module-namespace/creating.ts
@@ -0,0 +1,9 @@
+//tudo que exportar do namespace vira um atributo de um objeto namespace
+namespace helloWorld {
+	export class namePerson {
+		constructor(private name: string) {}
+	}
+	export const monki = new namePerson("monki");
+}
+const monki = new helloWorld.namePerson("monki");
+const monki2 = helloWorld.monki;
