0566762c418e29e7b255290bb67e90b61501c47d
albqvictor1508 <albq.victor@gmail.com>
2025-05-03 14:25:52 -0300

    dip concept

---
+++ TS/src/SOLID/src/dip/index.ts
@@ -0,0 +1,11 @@
+/*
+Dependency Inversion
+
+- Módulos de alto nível não devem depender de módulos de baixo nível. Ambos devem depender de abstrações.
+Dependa de abstrações, não de implementações.
+Abstrações não devem depender de detalhes, Detalhes devem depender de abstrações
+
+Classes de baixo nível são classes que implementam as tarefas (os detalhes)
+Classes de alto nível são classes que gerenciam classes de baixo nível.
+
+*/
