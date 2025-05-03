e823d989be153b66b05a71cc0ab96f2a22ca062a
albqvictor1508 <albq.victor@gmail.com>
2025-05-03 14:31:27 -0300

    dip

--- TS/src/SOLID/src/dip/index.ts
+++ TS/src/SOLID/src/dip/index.ts
@@ -5,7 +5,33 @@ Dependency Inversion
 Dependa de abstrações, não de implementações.
 Abstrações não devem depender de detalhes, Detalhes devem depender de abstrações
 
-Classes de baixo nível são classes que implementam as tarefas (os detalhes)
+Classes de baixo nível são classes que implementam as tarefas (os detalhes)\
 Classes de alto nível são classes que gerenciam classes de baixo nível.
 
+Nesse caso é bom lembrar do padrão de projeto Strategy, que utiliza muito disso
 */
+export class Car {
+    name: string;
+    year: number;
+    color: string;
+}
+
+export class Ferrari extends Car {
+    //ferrari
+}
+
+export class Bugatti extends Car {
+    //bugatti
+}
+
+class Driver {
+    name: string;
+    age: number;
+    car: Car;
+
+    constructor(name: string, age: number, car: Car) {
+        this.car = car;
+        this.age =age;
+        this.name = name;
+    }
+}
