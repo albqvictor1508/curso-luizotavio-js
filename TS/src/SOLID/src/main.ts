6d5a6493c1147f1f90975ad73d5f1c01e1c48135
albqvictor1508 <albq.victor@gmail.com>
2025-05-03 14:19:05 -0300

    more about isp

--- TS/src/SOLID/src/main.ts
+++ TS/src/SOLID/src/main.ts
@@ -4,21 +4,25 @@ import { ShoppingCart } from "./shopping-principle";
 import { Persistency } from "./srp/persistency";
 import { NoDiscount } from "./ocp/discount";
 import { Product } from "./srp/product";
+import { EnterpriseCostumer, IndividualCostumer } from "./isp/costumer";
 
 const discount = new NoDiscount();
 const cart = new ShoppingCart(discount);
 const persistency = new Persistency();
 const messaging = new Messaging();
-const order = new Order(cart, messaging);
-<<<<<<< HEAD
+const individualCostumer = new IndividualCostumer(
+    "victor", "arruda", "000000"
+);
+const enterpriseCostumer = new EnterpriseCostumer(
+    "pitoco aws", "victor", "12345"
+)
+const order = new Order(cart, messaging, individualCostumer);
 cart.addItem(new Product("teste 1", 120));
 cart.addItem(new Product("teste 2", 240));
 cart.addItem(new Product("teste 3", 1000));
-=======
 cart.addItem(new Product("test product", 1200));
 cart.addItem(new Product("test product 2", 500));
 cart.addItem(new Product("test product 3", 200));
->>>>>>> 1862a57b2a5a9a054a4a7235dfd9185788bbd39e
 cart.removeItem(0);
 console.log(cart.showCart());
 console.log("total: ", cart.total());
--- TS/src/SOLID/src/srp/order.ts
+++ TS/src/SOLID/src/srp/order.ts
@@ -1,10 +1,11 @@
 import type { OrderStatus } from "../types";
 import type { Messaging } from "./messaging";
 import type { ShoppingCart } from "../shopping-principle";
+import type { CostumerOrder } from "../isp/costumer/costumer-protocol";
 
 export class Order {
   private _orderStatus: OrderStatus = "open"
-  constructor(private readonly cart: ShoppingCart, private readonly messaging: Messaging) {}
+  constructor(private readonly cart: ShoppingCart, private readonly messaging: Messaging, private readonly costumer: CostumerOrder) {}
 
   get orderStatus() :OrderStatus {
     return this._orderStatus;
@@ -17,7 +18,8 @@ export class Order {
 
     this._orderStatus = "closed";
     this.messaging.sendMessage("pedido foi enviado com sucesso");
-    // this.saveOrder();
     this.cart.clear();
+    console.log("O cliente é: " + this.costumer.getName());
+    
   }
 }
