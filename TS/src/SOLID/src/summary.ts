df506961b8e3d7c4a4bb78f0a9187bacabb3d49b
albqvictor1508 <albq.victor@gmail.com>
2025-05-03 14:55:02 -0300

    yagni and kiss

--- TS/src/SOLID/src/summary.ts
+++ TS/src/SOLID/src/summary.ts
@@ -6,7 +6,12 @@ Vantagens:
 - Código reutilizado
 - De acordo com o DRY (Don't Repeat Yourself)
 - Código testável, baixo acoplamento
+- Alta coesão
+- Expansível
 
 Desvantagens: 
-
+- Complexidade aumenta bastante
+- Quantidade de código aumenta
+- YAGNI (You aren't gonna need this) = tipo os cara que coloca microserviço em software pequeno com pouco dev
+- KISS ()
 */
