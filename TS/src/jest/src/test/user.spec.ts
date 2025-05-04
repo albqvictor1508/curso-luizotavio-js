711133537d5bea44076637341853a86f7495af9b
albqvictor1508 <albq.victor@gmail.com>
2025-05-04 08:49:43 -0300

    silent test

--- TS/src/jest/src/test/user.spec.ts
+++ TS/src/jest/src/test/user.spec.ts
@@ -1,3 +1,5 @@
+// user.spec.ts => geralmente se usa spec para teste unitário e test para teste de integração
+
 describe("user tests", () => {
 	it("should return one", () => {
 		const number = 1;
@@ -9,5 +11,11 @@ describe("user tests 2", () => {
 	it("should return my name", () => {
 		const myName = "my name";
 		expect(myName).toBe("my name");
+	});
+});
+
+describe("primitive values", () => {
+	it("should test jest assertions", () => {
+		const number = 10;
 	});
 });
