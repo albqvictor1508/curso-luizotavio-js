38298ccd5e57daf41123f430f0dba99398c654ec
albqvictor1508 <albq.victor@gmail.com>
2025-05-04 08:51:40 -0300

    to equal (with Object.is)

--- TS/src/jest/src/test/user.spec.ts
+++ TS/src/jest/src/test/user.spec.ts
@@ -17,7 +17,6 @@ describe("user tests 2", () => {
 describe("primitive values", () => {
 	it("should test jest assertions", () => {
 		const number = 10;
-
-		expect(number).toBe(10);
+		expect(number).toEqual(10);
 	});
 });
