037c3db9f303868c348e95ab090d2188fc1c8ebb
albqvictor1508 <albq.victor@gmail.com>
2025-05-05 19:39:09 -0300

    salve

---
+++ TS/src/design-patterns/src/strategy/index.ts
@@ -0,0 +1,24 @@
+class Car {
+	constructor(
+		private name: string,
+		private age: number,
+	) {}
+
+	accelerate(): void {
+		console.log("accelerating...");
+	}
+}
+
+class Celta extends Car {}
+
+class Marea extends Car {}
+
+class Nivus extends Car {}
+
+class Pilot {
+	private car: Car;
+
+	constructor(car: Car) {
+		this.car = car;
+	}
+}
