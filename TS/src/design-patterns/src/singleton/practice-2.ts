f1f7a5ceb5eae57a646b94d1cd14057f12e61541
albqvictor1508 <albq.victor@gmail.com>
2025-05-06 15:59:09 -0300

    practice 2 initial commit

--- TS/src/design-patterns/src/singleton/practice-2.ts
+++ TS/src/design-patterns/src/singleton/practice-2.ts
@@ -3,5 +3,10 @@ class Practice2 {
 
 	private constructor() {}
 
-	public static getInstance() {}
+	public static getInstance() {
+		if (Practice2.instance === null) {
+			Practice2.instance = new Practice2();
+		}
+		return Practice2.instance;
+	}
 }
