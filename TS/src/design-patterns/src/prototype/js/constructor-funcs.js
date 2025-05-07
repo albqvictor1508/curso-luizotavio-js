4acd02a1051a276d62663f096a380f2222b76dd7
albqvictor1508 <albq.victor@gmail.com>
2025-05-07 16:13:58 -0300

    prototype

--- TS/src/design-patterns/src/prototype/js/constructor-funcs.js
+++ TS/src/design-patterns/src/prototype/js/constructor-funcs.js
@@ -3,4 +3,16 @@ function Person(name, age) {
 	this.age = age;
 	return this;
 }
+
+const personPrototype = {
+	firstName: "victor",
+	lastName: "albuquerque",
+	age: 18,
+	fullName() {
+		return `${this.firstName} ${this.lastName}`;
+	},
+};
+
 Person.prototype = Object.create(personPrototype);
+
+const person1 = new Person("João", "Miranda", 18);
