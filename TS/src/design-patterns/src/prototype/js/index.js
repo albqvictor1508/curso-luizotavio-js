6a0dc1b4147f9b82701df1b4fbaa31dd4e250bd5
albqvictor1508 <albq.victor@gmail.com>
2025-05-07 16:00:27 -0300

    prototype initial

---
+++ TS/src/design-patterns/src/prototype/js/index.js
@@ -0,0 +1,8 @@
+const person = {
+	firstName: "victor",
+	lastName: "albuquerque",
+	age: 18,
+	fullName: () => `${this.firstName} ${this.lastName}`,
+};
+
+const anotherPerson = Object.create(person);
