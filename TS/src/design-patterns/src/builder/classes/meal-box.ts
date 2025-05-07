493e9d6aaa3e41012428623cde05fa512e29f235
albqvictor1508 <albq.victor@gmail.com>
2025-05-07 12:00:32 -0300

    more

---
+++ TS/src/design-patterns/src/builder/classes/meal-box.ts
@@ -0,0 +1,19 @@
+import type { MealCompositeProtocol } from "../pratice-2";
+
+export class MealBox implements MealCompositeProtocol {
+	private readonly children: MealCompositeProtocol[] = [];
+
+	getPrice(): number {
+		const totalPrice: number = this.children.reduce(
+			(count, m) => m.getPrice() + count,
+			0,
+		);
+		return totalPrice;
+	}
+
+	add(...meal: MealCompositeProtocol[]) {
+		for (const m of meal) {
+			this.children.push(m);
+		}
+	}
+}
