eb12731948f6c73e61cc356574e0ae6410b4d8ad
albqvictor1508 <albq.victor@gmail.com>
2025-05-07 11:55:17 -0300

    more food

---
+++ TS/src/design-patterns/src/builder/classes/meals.ts
@@ -0,0 +1,9 @@
+import { AbstractMeal } from "./abstract-meal";
+
+export class Rice extends AbstractMeal {}
+
+export class Beans extends AbstractMeal {}
+
+export class Meat extends AbstractMeal {}
+
+export class Desert extends AbstractMeal {}
