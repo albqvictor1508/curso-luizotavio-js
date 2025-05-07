9b5a7a29262aa783939a2c3bd63595427c8c3107
albqvictor1508 <albq.victor@gmail.com>
2025-05-07 16:23:04 -0300

    a little about prototype

--- TS/src/design-patterns/src/prototype/method1/prototype-1.ts
+++ TS/src/design-patterns/src/prototype/method1/prototype-1.ts
@@ -1,1 +1,14 @@
-console.log("salve");
+export interface Prototype {
+	clone(): Prototype;
+}
+
+export class Person implements Prototype {
+	constructor(
+		public name: string,
+		public age: number,
+	) {}
+	clone(): Prototype {
+		const newObject = Object.create(this);
+		return newObject;
+	}
+}
