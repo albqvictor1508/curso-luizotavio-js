53a45596783f33d427299206f3a4a969f1d95376
albqvictor1508 <albq.victor@gmail.com>
2025-05-13 10:43:18 -0300

    example car factory

--- TS/src/design-patterns/src/factory-method/example.ts
+++ TS/src/design-patterns/src/factory-method/example.ts
@@ -1,2 +1,14 @@
 export type Car = { model: string; engine: string };
-export type carFactory = { showDetails(): void };
+export type CarPrototype = { showDetails(): void };
+
+const carPrototype: CarPrototype = {
+	showDetails: (): void => {
+		console.log(this);
+	},
+};
+
+const carFactory = (model: string, engine: string): Car & CarPrototype => {
+	const idAsPrivateMember = Math.floor(Math.random() * 1000);
+	const carObj = Object.create(carPrototype);
+	return Object.assign(carObj, { id: idAsPrivateMember, model, engine });
+};
