368e5f989a4bf8f4fb351e2ab01c0a04c4b6fb26
albqvictor1508 <albq.victor@gmail.com>
2025-05-14 09:44:06 -0300

    customer and vehicle

--- TS/src/design-patterns/src/abstract-factory/practice/customer/enterprise-customer.ts
+++ TS/src/design-patterns/src/abstract-factory/practice/customer/enterprise-customer.ts
@@ -1,8 +1,5 @@
 import type { Customer } from "./index";
 
 export class EnterpriseCustomer implements Customer {
-	name: string;
-	constructor(name: string) {
-		this.name = name;
-	}
+	constructor(public name: string) {}
 }
