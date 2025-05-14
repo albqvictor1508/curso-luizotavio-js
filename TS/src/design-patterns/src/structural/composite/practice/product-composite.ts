04630f7c4ee4460e2323815a35442d57549f4ceb
albqvictor1508 <albq.victor@gmail.com>
2025-05-14 16:04:27 -0300

    product composite

--- TS/src/design-patterns/src/structural/composite/practice/product-composite.ts
+++ TS/src/design-patterns/src/structural/composite/practice/product-composite.ts
@@ -3,3 +3,37 @@ export abstract class ProductComponent {
 	add(product: ProductComponent): void {}
 	remove(product: ProductComponent): void {}
 }
+
+export class ProductLeaf extends ProductComponent {
+	constructor(
+		private name: string,
+		private price: number,
+	) {
+		super();
+	}
+	getPrice(): number {
+		return this.price;
+	}
+
+	getName(): string {
+		return this.name;
+	}
+}
+
+export class ProductComposite extends ProductComponent {
+	private children: ProductComponent[] = [];
+
+	getPrice(): number {
+		return this.children.reduce((count, p) => p.getPrice() + count, 0);
+	}
+
+	add(product: ProductComponent): void {
+		this.children.push(product);
+	}
+
+	remove(product: ProductComponent): void {
+		const productIndex: number = this.children.indexOf(product);
+		if (!productIndex) return;
+		this.children.splice(productIndex - 1, 1);
+	}
+}
