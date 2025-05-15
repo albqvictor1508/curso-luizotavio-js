9b04d5fc7584edd3ec30e57677202eb4080b1a3c
albqvictor1508 <albq.victor@gmail.com>
2025-05-15 11:19:04 -0300

    remote control with volume volume up

---
+++ TS/src/design-patterns/src/structural/brigde/remote-control/remote-control.with-volume.ts
@@ -0,0 +1,7 @@
+import { RemoteControl } from "./index";
+
+export class RemoteControlWithVolume extends RemoteControl {
+	public volumeUp(value: number): void {
+		this.device.setVolume(this.device.getVolume() + value);
+	}
+}
