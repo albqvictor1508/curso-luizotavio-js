6c288c08cf39e44b00e00a8d400fa4123928d5a1
albqvictor1508 <albq.victor@gmail.com>
2025-05-15 11:13:27 -0300

    remote control

--- TS/src/design-patterns/src/structural/brigde/remote-control/index.ts
+++ TS/src/design-patterns/src/structural/brigde/remote-control/index.ts
@@ -1,3 +1,12 @@
+import type { DeviceImpl } from "../device";
+
 export class RemoteControl {
-	constructor(protected device: Device) {}
+	constructor(protected device: DeviceImpl) {}
+
+	togglePower(): void {
+		this.device.setPower(!this.device.getPower());
+		console.log(
+			`${this.device.getName()} está ${this.device.getPower() ? "ligado" : "desligado"}`,
+		);
+	}
 }
