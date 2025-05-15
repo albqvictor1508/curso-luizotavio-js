1ba3cdb3a2857c95282635f894ee9340ee3f2a72
albqvictor1508 <albq.victor@gmail.com>
2025-05-15 11:33:11 -0300

    client code, part 2

--- TS/src/design-patterns/src/structural/brigde/main.ts
+++ TS/src/design-patterns/src/structural/brigde/main.ts
@@ -1,7 +1,7 @@
 import { Radio } from "./device/radio";
 import { Tv } from "./device/tv";
-import type { RemoteControl } from "./remote-control";
-import type { RemoteControlWithVolume } from "./remote-control/remote-control.with-volume";
+import { RemoteControl } from "./remote-control";
+import { RemoteControlWithVolume } from "./remote-control/remote-control.with-volume";
 
 const main = (abstraction: RemoteControl | RemoteControlWithVolume) => {
 	abstraction.togglePower();
@@ -13,3 +13,5 @@ const main = (abstraction: RemoteControl | RemoteControlWithVolume) => {
 
 const tv = new Tv();
 const radio = new Radio();
+const radioRemoteControl = new RemoteControl(radio);
+const tvRemoteControl = new RemoteControlWithVolume(tv);
