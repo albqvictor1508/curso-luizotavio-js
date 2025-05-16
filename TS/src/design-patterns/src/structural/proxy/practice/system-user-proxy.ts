32d1387878ecd33fa68558fafda624c1e79ddec0
albqvictor1508 <albq.victor@gmail.com>
2025-05-16 11:30:07 -0300

    system user proxy

--- TS/src/design-patterns/src/structural/proxy/practice/system-user-proxy.ts
+++ TS/src/design-patterns/src/structural/proxy/practice/system-user-proxy.ts
@@ -1,6 +1,9 @@
 import type { UserAddress, UserProtocol } from "./user/user-protocol";
 
 export class SystemUserProxy implements UserProtocol {
+	private readonly user: UserProtocol | null = null;
+	private readonly userAdresses: UserAddress[] | null = null;
+
 	constructor(public username: string) {}
 	getAdresses(): Promise<UserAddress[]> {
 		throw new Error("Method not implemented.");
