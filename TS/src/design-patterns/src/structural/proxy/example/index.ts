3ba3797af5f35f64cfa31fd30f2404e68370e50f
albqvictor1508 <albq.victor@gmail.com>
2025-05-16 10:57:16 -0300

    making proxy example, part 2

--- TS/src/design-patterns/src/structural/proxy/example/index.ts
+++ TS/src/design-patterns/src/structural/proxy/example/index.ts
@@ -7,3 +7,12 @@ export class ExampleRealSubject implements ExampleSubject {
 		console.log("Algo que meu objeto faz");
 	}
 }
+
+export class ExampleProxy implements ExampleSubject {
+	constructor(private subject: ExampleSubject) {}
+	request(): void {
+		console.log("meu proxy faz algo");
+		this.subject.request();
+		console.log("meu proxy agindo novamente");
+	}
+}
