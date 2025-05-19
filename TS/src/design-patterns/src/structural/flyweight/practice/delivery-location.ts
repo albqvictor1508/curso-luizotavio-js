e80fa58ec98d8d77c91521c319f334f8c4a1c082
albqvictor1508 <albq.victor@gmail.com>
2025-05-19 19:52:46 -0300

    flyweight practice, part 2

--- TS/src/design-patterns/src/structural/flyweight/practice/delivery-location.ts
+++ TS/src/design-patterns/src/structural/flyweight/practice/delivery-location.ts
@@ -1,6 +1,12 @@
-import {DeliveryFlyweight} from "./index.ts";
+import { DeliveryLocationData } from "./delivery-types.js";
+import type {DeliveryFlyweight} from "./index.ts";
 
 export class DeliveryLocation implements DeliveryFlyweight {
-  constructor(private readonly intrinsicState) {};
+  constructor(private readonly intrinsicState: DeliveryLocationData) {}
+  
+  deliver(name: string, number: number): void {
+    console.log("Entrega finalizada!");
+  }
+;
 
 }
