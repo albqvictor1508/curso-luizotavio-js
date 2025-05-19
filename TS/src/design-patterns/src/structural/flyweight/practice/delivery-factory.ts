f230cc31aeeef792576efa2962b5284671d2fa29
albqvictor1508 <albq.victor@gmail.com>
2025-05-19 20:18:59 -0300

    flyweight practice, part 4

--- TS/src/design-patterns/src/structural/flyweight/practice/delivery-factory.ts
+++ TS/src/design-patterns/src/structural/flyweight/practice/delivery-factory.ts
@@ -1,4 +1,5 @@
 import { DeliveryFlyweight } from ".";
+import { DeliveryLocation } from "./delivery-location";
 import { DeliveryLocationData, DeliveryLocationDictionary } from "./delivery-types";
 
 export class DeliveryFactory {
@@ -9,6 +10,9 @@ export class DeliveryFactory {
     }
 
     makeLocation(intrinsicState: DeliveryLocationData): DeliveryFlyweight {
-        if(d)
+        const id = this.createId(intrinsicState);
+        if(id in this.locations) return this.locations.get(id) as DeliveryFlyweight;
+        this.locations.set(id, new DeliveryLocation(intrinsicState));
+        return this.locations.get(id) as DeliveryFlyweight; 
     }
 }
