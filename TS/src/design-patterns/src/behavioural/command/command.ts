7a87c5031fc22abca430e6555d283c74f25a02c4
albqvictor1508 <albq.victor@gmail.com>
2025-05-21 16:23:58 -0300

    command practice, part 6

--- TS/src/design-patterns/src/behavioural/command/command.ts
+++ TS/src/design-patterns/src/behavioural/command/command.ts
@@ -1,4 +1,16 @@
+import { SmartHouseLight } from "./practice";
+
 export interface SmartHouseCommand {
     execute(): void;
     undo(): void;
+}
+
+export class LightPowerCommand implements SmartHouseCommand {
+    constructor(private readonly light:SmartHouseLight) {}
+    execute(): void {
+        this.light.toggle();
+    }
+    undo(): void {
+        this.light.toggle();
+    }
 }
