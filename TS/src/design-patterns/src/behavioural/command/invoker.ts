497f6ca3e029de4a13ec134201a928dea22e77bc
albqvictor1508 <albq.victor@gmail.com>
2025-05-21 16:28:32 -0300

    command practice, part 8

--- TS/src/design-patterns/src/behavioural/command/invoker.ts
+++ TS/src/design-patterns/src/behavioural/command/invoker.ts
@@ -3,7 +3,13 @@ import { SmartHouseCommand } from "./command";
 export class SmartHouseApp {
     private commands: Map<string, SmartHouseCommand> = new Map();
     
-    addComand(key: string, command: SmartHouseCommand) {
+    addComand(key: string, command: SmartHouseCommand): void {
         this.commands.set(key, command);
     }
+
+    executeCommand(key: string): void {
+        this.commands.get(key)?.execute();
+    }
+
+
 }
